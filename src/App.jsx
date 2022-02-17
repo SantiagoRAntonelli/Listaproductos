5e015e1f1b61269b707e61deddf604a6e34cb600
santyanto <santy_antonelli@hotmail.com>
2022-02-17 09:09:54 -0300

    lista1

---
+++ src/App.jsx
@@ -0,0 +1,107 @@
+import './App.css';
+import { Cards } from './components/Cards';
+import  laptop  from './laptop.jpg';
+function App() {
+  return (
+    <div className="textomadre">
+    <div className="col-12"> ALGO MAS</div>
+    <div className="row">
+    <div className="col-sm-3 order-sm-1 mb-3">
+      Proximamente Filtros
+    </div>
+    <div className="col-sm-2 order-sm-3 mb-3">
+      Proximamente Carrito
+    </div>
+    <div className="col-sm-7 order-sm-2 px-5"> 
+            <div className="card mb-3">
+              <div className="row">
+                <div className="col-sm-4">
+                  <img className="imagenes" src="https://media.istockphoto.com/photos/modern-laptop-with-empty-screen-on-white-background-mockup-design-picture-id1182241805?s=612x612"/>
+                </div>
+                <div className="col-sm-8">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div className="card mb-3">
+              <div className="row">
+                <div className="col-sm-4">
+                    <img src="/home/santi/Escritorio/Proyectos/listaproductos/src/laptopprueba.jpg"/>
+                </div>
+                <div className="col-sm-8">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div className="card mb-3">
+              <div className="row">
+                <div className="col-sm-4">
+                    <img src="/home/santi/Escritorio/Proyectos/listaproductos/src/laptopprueba.jpg"/>
+                </div>
+                <div className="col-sm-8">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div className="card mb-3">
+              <div className="row">
+                <div className="col-sm-4">
+                    <img src="/home/santi/Escritorio/Proyectos/listaproductos/src/laptopprueba.jpg"/>
+                </div>
+                <div className="col-sm-8">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div className="card mb-3">
+              <div className="row">
+                <div className="col-sm-4">
+                    <img src="/home/santi/Escritorio/Proyectos/listaproductos/src/laptopprueba.jpg"/>
+                </div>
+                <div className="col-sm-8">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div className="card mb-3">
+              <div className="row">
+                <div className="col-sm-4">
+                    <img src="/home/santi/Escritorio/Proyectos/listaproductos/src/laptopprueba.jpg"/>
+                </div>
+                <div className="col-sm-8">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div className="card mb-3">
+              <div className="row">
+                <div className="col-sm-4">
+                    <img src="/home/santi/Escritorio/Proyectos/listaproductos/src/laptopprueba.jpg"/>
+                </div>
+                <div className="col-sm-8">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    </div>
+                </div>
+              </div>
+            </div>
+    </div>
+   
+    </div>
+    </div>
+  );
+}
+
+export default App;
