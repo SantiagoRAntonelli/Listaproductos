946d384f54f46674aeebb8e709912e277f736405
santyanto <santy_antonelli@hotmail.com>
2022-02-17 09:35:25 -0300

    conobjetos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
-import { Cards } from './components/Cards';
-import  laptop  from './laptop.jpg';
+import { Plantilla } from './components/Plantilla.js'
+
+
 function App() {
   return (
     <div className="textomadre">
@@ -13,92 +14,13 @@ function App() {
       Proximamente Carrito
     </div>
     <div className="col-sm-7 order-sm-2 px-5"> 
-            <div className="card mb-3">
-              <div className="row">
-                <div className="col-sm-4">
-                  <img className="imagenes" src="https://media.istockphoto.com/photos/modern-laptop-with-empty-screen-on-white-background-mockup-design-picture-id1182241805?s=612x612"/>
-                </div>
-                <div className="col-sm-8">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    </div>
-                </div>
-              </div>
-            </div>
-            <div className="card mb-3">
-              <div className="row">
-                <div className="col-sm-4">
-                    <img src="/home/santi/Escritorio/Proyectos/listaproductos/src/laptopprueba.jpg"/>
-                </div>
-                <div className="col-sm-8">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    </div>
-                </div>
-              </div>
-            </div>
-            <div className="card mb-3">
-              <div className="row">
-                <div className="col-sm-4">
-                    <img src="/home/santi/Escritorio/Proyectos/listaproductos/src/laptopprueba.jpg"/>
-                </div>
-                <div className="col-sm-8">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    </div>
-                </div>
-              </div>
-            </div>
-            <div className="card mb-3">
-              <div className="row">
-                <div className="col-sm-4">
-                    <img src="/home/santi/Escritorio/Proyectos/listaproductos/src/laptopprueba.jpg"/>
-                </div>
-                <div className="col-sm-8">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    </div>
-                </div>
-              </div>
-            </div>
-            <div className="card mb-3">
-              <div className="row">
-                <div className="col-sm-4">
-                    <img src="/home/santi/Escritorio/Proyectos/listaproductos/src/laptopprueba.jpg"/>
-                </div>
-                <div className="col-sm-8">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    </div>
-                </div>
-              </div>
-            </div>
-            <div className="card mb-3">
-              <div className="row">
-                <div className="col-sm-4">
-                    <img src="/home/santi/Escritorio/Proyectos/listaproductos/src/laptopprueba.jpg"/>
-                </div>
-                <div className="col-sm-8">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    </div>
-                </div>
-              </div>
-            </div>
-            <div className="card mb-3">
-              <div className="row">
-                <div className="col-sm-4">
-                    <img src="/home/santi/Escritorio/Proyectos/listaproductos/src/laptopprueba.jpg"/>
-                </div>
-                <div className="col-sm-8">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    </div>
-                </div>
-              </div>
-            </div>
+            <Plantilla/>
+            <Plantilla/>
+            <Plantilla/>
+            <Plantilla/>
+            <Plantilla/>
+            <Plantilla/>
     </div>
-   
     </div>
     </div>
   );
---
+++ src/components/Plantilla.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Plantilla = () => {
+  return (
+    <div className="card mb-3">
+              <div className="row">
+                <div className="col-sm-4">
+                  <img className="imagenes" src="https://media.istockphoto.com/photos/modern-laptop-with-empty-screen-on-white-background-mockup-design-picture-id1182241805?s=612x612"/>
+                </div>
+                <div className="col-sm-8">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    </div>
+                </div>
+              </div>
+            </div>
+  )
+}
